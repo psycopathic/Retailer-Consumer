17a08c400ca16158a135bde358edd3c3445252b5
psycopathic <harshsrivastava2502@gmail.com>
2025-12-07 00:11:43 +0530

    revisite

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -11,7 +11,7 @@ const poppins = Poppins({
 const page = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-80px)] text-center px-4">
-      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div></div>
+      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-size-[6rem_4rem]"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_800px_at_100%_200px,#d5c5ff,transparent)]"></div></div>
       <h1 className={`text-6xl font-bold font-playwrite-nz`}>
         The Ultimate Platform for <span className="text-primary">E-commerce</span>
       </h1>
